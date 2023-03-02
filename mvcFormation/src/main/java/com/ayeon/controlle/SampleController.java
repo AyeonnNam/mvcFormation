e6f1e0262d1925557e548956f5cbf2c241cba6f4
nam-ayeon <boalove284@naver.com>
2023-03-02 20:51:06 +0900

    @RequestParam

--- mvcFormation/src/main/java/com/ayeon/controlle/SampleController.java
+++ mvcFormation/src/main/java/com/ayeon/controlle/SampleController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ayeon.domain.SampleDTO;
 
 import lombok.extern.slf4j.Slf4j;
 @Controller
@@ -27,4 +30,21 @@ public class SampleController {
 		log.info("basic get only get......");
 	}
 	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		
+		log.info("ex01-------dto : " + dto);
+		
+		return "ex01";
+	}
+	
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		
+		log.info("------- name: " + name);
+		log.info("------- age: " + age);
+		return "ex02";
+
+	}
 }
---
+++ mvcFormation/src/main/java/com/ayeon/domain/SampleDTO.java
@@ -0,0 +1,11 @@
+package com.ayeon.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+
+	
+	private String name;
+	private int age;
+}
