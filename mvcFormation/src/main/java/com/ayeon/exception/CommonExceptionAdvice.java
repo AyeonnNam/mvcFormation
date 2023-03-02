f808dc76f79dea090a514a1604d603e05c62b3e4
nam-ayeon <boalove284@naver.com>
2023-03-03 00:48:38 +0900

    NoHandlerFoundException.class

--- mvcFormation/src/main/java/com/ayeon/exception/CommonExceptionAdvice.java
+++ mvcFormation/src/main/java/com/ayeon/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package com.ayeon.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +16,17 @@ public class CommonExceptionAdvice {
 
 	@ExceptionHandler(Exception.class)
 	public String except(Exception ex, Model model) {
-		
+
 		log.error("Exception..." + ex.getMessage());
 		model.addAttribute("exception", ex);
 		log.error("model: ------" + model);
 		return "error_page";
 	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException ex) {
+		return "custom404";
+	}
+
 }
---
+++ mvcFormation/src/main/webapp/WEB-INF/views/custom404.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD THML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page session="false" import="java.util.*"%>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset="UTF-8">
+
+</head>
+<body>
+
+	<h1>해당url은존재하지않는다
+	</h1>
+</body>
+</html>
