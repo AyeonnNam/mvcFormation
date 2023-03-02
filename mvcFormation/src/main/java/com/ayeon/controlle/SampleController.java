c776ffef22be1868ace40fe4e388a7631766109e
nam-ayeon <boalove284@naver.com>
2023-03-02 23:54:35 +0900

    @PostMapping

--- mvcFormation/src/main/java/com/ayeon/controlle/SampleController.java
+++ mvcFormation/src/main/java/com/ayeon/controlle/SampleController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ayeon.domain.SampleDTO;
 import com.ayeon.domain.SampleDTOList;
@@ -141,5 +143,22 @@ public class SampleController {
 			
 	}
 	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload....................");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		
+		files.forEach(file ->{
+			
+			log.info("-------------");
+			log.info("name:" + file.getOriginalFilename());
+			log.info("size" + file.getSize());
+			
+		});
+		
+	}
 	
 }
---
+++ mvcFormation/src/main/webapp/WEB-INF/views/sample/exUpload.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD THML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset="UTF-8">
+
+</head>
+<body>
+
+<form action="/sample/exUploadPost" method="post" enctype="multipart/form-data">
+<div>
+<input type='file' name='files'></div>
+<input type='file' name='files'></div>
+
+<input type='file' name='files'></div>
+<div>
+<input type='submit'>
+</div>
+
+</form>
+</body>
+</html>
